b590c922a85405eb02f3391bc91c1ca214f26e62
nagasrihith608 <nagasrihith608@gmail.com>
2023-03-21 11:37:24 +0530

    Calculator file created

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,84 @@
+public class Calculator {
+//    private static final Logger logger = LogManager.getLogger(Calculator.class);
+    public Calculator() {
+//        logger.info("Calculator Initialised");
+    }
+
+    public Double squareRoot(double x){
+        Double result = 0.0;
+        try
+        {
+            if(x < 0)
+            {
+                result = null;
+                throw new Exception("The input for square root cannot be negative");
+            }
+            else
+            {
+                result = Math.sqrt(x);
+
+            }
+        } catch (Exception e) {
+//            logger.error(e);
+        }
+
+
+        return result;
+    }
+
+    public Double factorial(double x)
+    {
+        Double result = 0.0;
+        try
+        {
+            if(x < 0)
+            {
+                result = null;
+                throw new Exception("The input for square root cannot be negative");
+            }
+            else
+            {
+                result = 1.0;
+                for(int i = 2;i <= x;i++)
+                {
+                    result = result*i;
+                }
+
+            }
+        } catch (Exception e) {
+//            logger.error(e);
+        }
+
+
+        return result;
+    }
+
+    public Double logarithm(double x)
+    {
+        Double result = 0.0;
+        try
+        {
+            if(x < 0)
+            {
+                result = null;
+                throw new Exception("The input for Logarithm cannot be negative");
+            } else if (x == 0) {
+                result = null;
+                throw new Exception("The input for Logarithm cannot be 0");
+            } else
+            {
+                result = Math.log(x);
+
+            }
+        } catch (Exception e) {
+//            logger.error(e);
+        }
+
+        return result;
+    }
+
+    public Double power(double x, double y)
+    {
+        return Math.pow(x, y);
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,59 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Scientific Calculator, Please enter your choice1");
+        System.out.println("0. Exit");
+        System.out.println("1. Calculate Square root function");
+        System.out.println("2. Factorial Function");
+        System.out.println("3. Natural Logarithm(base e)");
+        System.out.println("4. Power function");
+
+        Scanner sc = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+
+        int op = sc.nextInt();
+        double num1, num2;
+
+        while(op != 0)
+        {
+            switch (op)
+            {
+                case 1:
+                    System.out.println("Enter a number");
+                    num1 = sc.nextDouble();
+                    System.out.println("The Square root of " + num1 + " is " + calculator.squareRoot(num1));
+                    break;
+
+                case 2:
+                    System.out.println("Enter a number");
+                    num1 = sc.nextDouble();
+                    System.out.println("The factorial of " + num1 + " is " + calculator.factorial(num1));
+                    break;
+
+
+                case 3:
+                    System.out.println("Enter a number");
+                    num1 = sc.nextDouble();
+                    System.out.println("The Natural Logarithm of " + num1 + " is " + calculator.logarithm(num1));
+                    break;
+
+                case 4:
+                    System.out.println("Enter a number for the base");
+                    num1 = sc.nextDouble();
+                    System.out.println("Enter a number for the power");
+                    num2 = sc.nextDouble();
+                    System.out.println("The Value of " + num1 + " to the power of " + num2 + " is " + calculator.power(num1, num2));
+                    break;
+
+                default:
+//                    logger.error("Invalid Choice");
+                    break;
+            }
+
+            op = sc.nextInt();
+        }
+//        logger.info("Exiting Calculator");
+
+    }
+}
